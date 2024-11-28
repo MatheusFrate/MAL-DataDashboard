6fea4eddbff0bbe279b5e7b6d2cc5c6e4a5dc738
Matheus Frate <matheusbfrate@gmail.com>
2024-11-28 11:57:34 -0300

    build e outras configurações

--- MAL_project/settings.py
+++ MAL_project/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
